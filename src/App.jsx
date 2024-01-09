a04dd6179f676646468f8595ff2684e2cf52e408
EmanFun <emanuelfun.95@gmail.com>
2024-01-09 04:54:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import {Contact} from './Components/Modules/Contact'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main id='root'>
+    <section id='vacio'>
+    </section>
+    <Contact/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Modules/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+//import React from "react";
+import {divider, container} from './Contact.module.css';
+
+
+
+const Contact = () => {
+  return (<>
+    <section className={divider}>
+
+    </section>
+    <section className={container}>
+      <div>
+        <form>
+          El Formulario
+        </form>
+      </div>
+    </section>
+  </>
+  );
+};
+
+export default Contact;
+
+
+
+
+
+
