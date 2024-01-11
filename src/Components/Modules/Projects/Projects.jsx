7acae77362649a3d59326b63f2a0e57b424f3646
EmanFun <emanuelfun.95@gmail.com>
2024-01-11 07:11:08 -0300

    restructuring and fix styles

--- src/Components/Modules/Projects/Projects.jsx
+++ src/Components/Modules/Projects/Projects.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { container, projects, projectCard } from './project.module.css';
 const projectsArray = [
-    'project1',
-    'project2', 
-    'project3',
+    {title:'project1', rol: 'rol', status: 'status'},
+    {title:'project2', rol: 'rol', status: 'status'}, 
+    {title:'project3', rol: 'rol', status: 'status'},
 ]
 
 const Projects = () => {
@@ -15,7 +15,9 @@ const Projects = () => {
                 {
                     projectsArray.map((project, index)=>{
                         return <div key={index} className={projectCard}>
-                                {project}
+                                  <h3>Title: {project.title}</h3>
+                                  <p>Rol: {project.rol}</p>
+                                  <p>Status:{project.status}</p>
                                 </div> 
                     })
                 }
